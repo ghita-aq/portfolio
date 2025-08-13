1afad59200f6e4ccb4b476b3d0fe4f4cae28791b
ghita <aqasbi.ghita@gmail.com>
2025-08-13 16:43:18 +0100

    feat: fix nav

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,6 +12,10 @@ module.exports = {
         "black-200": "#090325",
         "white-100": "#f3f3f3",
       },
+      fontFamily: {
+        'space-grotesk': ['Space Grotesk', 'Poppins', 'sans-serif'],
+        'poppins': ['Poppins', 'sans-serif'],
+      },
       boxShadow: {
         card: "0px 35px 120px -15px #211e35",
       },
